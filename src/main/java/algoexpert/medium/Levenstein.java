31efbb81a7194d5aeaaaa0a3e132852ff2b92ac7
Seb <jwstes2@gmail.com>
2020-09-30 01:17:49 +0200

    LRU Algorithm

--- src/main/java/algoexpert/medium/Levenstein.java
+++ src/main/java/algoexpert/medium/Levenstein.java
@@ -7,53 +7,34 @@ import java.util.stream.Collectors;
 public class Levenstein {
 
     public static int levenshteinDistance(String str1, String str2) {
-        int edits = 0;
-        char[] arr1 = str1.toCharArray();
-        char[] arr2 = str2.toCharArray();
-        int arr1length = arr1.length;
-        int arr2length = arr2.length;
-        List<Character> chars = str2.chars().mapToObj(c -> (char) c).collect(Collectors.toList());
-        for (char c : arr1) {
-           System.out.print(c);
-            if (chars.contains(c)) {
-                System.out.println(1);
-                chars.remove(new Character(c)); //remove matching char
-                if(chars.contains(c)){
-                    edits++;
+        int[][] edits = new int[str2.length() + 1][str1.length() + 1];
+        for (int i = 0; i < str2.length() + 1; i++) {
+            for (int j = 0; j < str1.length() + 1; j++) {
+                edits[i][j] = j;
+            }
+            edits[i][0] = i;
+        }
+        for (int i = 1; i < str2.length() + 1; i++) {
+            for (int j = 1; j < str1.length() + 1; j++) {
+                if (str2.charAt(i - 1) == str1.charAt(j - 1)) {
+                    edits[i][j] = edits[i - 1][j - 1];
+                } else {
+                    edits[i][j] = 1 + Math.min(edits[i - 1][j - 1], Math.min(edits[i - 1][j], edits[i][j - 1]));
                 }
-                continue;
-            }
-            if (arr1length > arr2length) {
-                System.out.println(2);
-                edits++;                    //remove
-                arr1length--;
-                continue;
-            }
-            if (arr1length < arr2length) {
-                System.out.println(3);
-                chars.add(c);
-                edits++;                  //add
-                arr1length++;
-                continue;
-            }
-           else{
-                System.out.println(4);
-                chars.add(c);           //substit
-                edits++;
             }
         }
- //      System.out.println(arr1length + " " + arr2.length);
-
-        edits += Math.abs(arr1length - arr2.length); //add inserts and deletes
-        return edits;
+        return edits[str2.length()][str1.length()];
     }
 
-    public static void main(String[] args) {
-//        System.out.println(levenshteinDistance("", "abc"));
-//        System.out.println(levenshteinDistance("levent", "leven"));
-//        System.out.println(levenshteinDistance("abd", "abc"));
-//        System.out.println(levenshteinDistance("kera", "fag"));
-        System.out.println(levenshteinDistance("biting", "mitten"));
 
+    public static void main(String[] args) {
+        System.out.println(levenshteinDistance("", ""));
+        System.out.println(levenshteinDistance("levent", "leven"));
+        System.out.println(levenshteinDistance("abd", "abc"));
+        System.out.println(levenshteinDistance("kera", "fag"));
+        System.out.println(levenshteinDistance("biting", "mitten"));
+        String b ="benyam";
+        System.out.println(b.substring(0,0));
+        System.out.println(b.substring(0,6));
     }
 }
